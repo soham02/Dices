b0e95d1456e4b809f1f0b5297f652d09c6bfa06a
soham mhatre <soham.mhatre97@gmail.com>
2018-06-21 05:02:09 -0400

    Commit

--- Project7/main.cpp
+++ Project7/main.cpp
@@ -6,7 +6,7 @@ int main()
 	int Q = 1;
 	int V = 0;
 	while (Q > 0) {
-		V = ((rand() % 6) + 1);
+		V = ((rand() % 6) + 1); //Add 1 to eliminate 0
 		std::cout << ("The value is ");
 		std::cout << V << std::endl;
 		std::cout << ("1 to continue || 0 to quit") << std::endl;
